3bd44057871ba3e552cab8277965a28cae0fae63
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-12-06 20:28:47 +0300

    [TASK-2] Add extra-item of another category

--- task1/task1_1/script.js
+++ task1/task1_1/script.js
@@ -1,8 +1,19 @@
 'use strict';
 
-const itemName = "Ноутбук ASUS Vivobook Go 15";
-const itemCount = 25;
-const itemCategory = "Электроника";
-const itemPrice = 53990;
+{
+  const itemName = "Ноутбук ASUS Vivobook Go 15";
+  const itemCount = 25;
+  const itemCategory = "Электроника";
+  const itemPrice = 53990;
 
-console.log('itemName: ', itemName);
+  console.log(`Наименование товара: ${itemName}`);
+}
+
+{
+  const itemName = "Настольная игра на ассоциации Имаджинариум";
+  const itemCount = 200;
+  const itemCategory = "Развлечения";
+  const itemPrice = 1099;
+  
+  console.log(`Наименование товара: ${itemName}`);
+}
--- task1/task1_2/script.js
+++ task1/task1_2/script.js
@@ -1,11 +1,25 @@
 'use strict';
 
-const itemName = "Ноутбук ASUS Vivobook Go 15";
-const itemCount = 25;
-const itemCategory = "Электроника";
-const itemPrice = 53990;
+{
+  const itemName = "Ноутбук ASUS Vivobook Go 15";
+  const itemCount = 25;
+  const itemCategory = "Электроника";
+  const itemPrice = 53990;
 
-const totalAmount = itemPrice * itemCount;
+  const totalAmount = itemPrice * itemCount;
 
-console.log('itemName: ', itemName);
-console.log('totalAmount: ', totalAmount);
+  console.log(`Наименование товара: ${itemName}`);
+  console.log(`Общая сумма товара: ${totalAmount}`);
+}
+
+{
+  const itemName = "Настольная игра на ассоциации Имаджинариум";
+  const itemCount = 200;
+  const itemCategory = "Развлечения";
+  const itemPrice = 1099;
+
+  const totalAmount = itemPrice * itemCount;
+
+  console.log(`Наименование товара: ${itemName}`);
+  console.log(`Общая сумма товара: ${totalAmount}`);
+}
