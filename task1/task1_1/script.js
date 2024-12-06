416fe83a9c7e342ee8246728c485a6c1d1a6d4c7
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-12-06 20:16:12 +0300

    [TASK 1] Add solutions for both subtasks 1_1 + 1_2

---
+++ task1/task1_1/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const itemName = "Ноутбук ASUS Vivobook Go 15";
+const itemCount = 25;
+const itemCategory = "Электроника";
+const itemPrice = 53990;
+
+console.log('itemName: ', itemName);
---
+++ task1/task1_2/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const itemName = "Ноутбук ASUS Vivobook Go 15";
+const itemCount = 25;
+const itemCategory = "Электроника";
+const itemPrice = 53990;
+
+const totalAmount = itemPrice * itemCount;
+
+console.log('itemName: ', itemName);
+console.log('totalAmount: ', totalAmount);
