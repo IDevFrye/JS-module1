3aa9f7bb3d4c6a26f090e8d9680484f33689a68a
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-12-07 21:45:30 +0300

    [TASK-5] Delete if

--- task5/hard_task_02.js
+++ task5/hard_task_02.js
@@ -1,10 +1,6 @@
 'use strict';
 
 function getMinNumber(first, second) {
-  if (typeof first !== "number" || typeof second !== "number") {
-    return "Вы ввели не число(а)!";
-  }
-
   return `Минимальное число из пары (${first}, ${second}) = ${(first + second - Math.abs(first - second)) / 2}`;
 }
 
@@ -12,4 +8,3 @@ function getMinNumber(first, second) {
 console.log(getMinNumber(1, 19));
 console.log(getMinNumber(21, 19));
 console.log(getMinNumber(-1, -19));
-console.log(getMinNumber(1, {}));
