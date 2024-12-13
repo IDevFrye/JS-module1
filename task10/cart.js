436a0490af6454de394a9fe9bd1771529364edb2
Andrey Kostin <andrey.kostin.111@mail.ru>
2024-12-14 00:59:01 +0300

    [TASK-10] Add solutions to Cart & IPv4

---
+++ task10/cart.js
@@ -0,0 +1,82 @@
+'use strict';
+
+let cart = {
+  items: [],
+  count: 0,
+
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+
+  add(name, price, quantity = 1) {
+    if (typeof name !== "string" || name.trim() === "") {
+      console.error("Ошибка: название товара должно быть непустой строкой.");
+      return;
+    }
+
+    if (typeof price !== "number" || price <= 0) {
+      console.error("Ошибка: цена товара должна быть положительным целым числом.");
+      return;
+    }
+
+    if (!Number.isInteger(quantity) || quantity <= 0) {
+      console.error("Ошибка: количество товара должно быть положительным целым числом.");
+      return;
+    }
+
+    this.items.push({
+      name,
+      price,
+      quantity,
+    });
+    this.increaseCount(quantity);
+  },
+
+  increaseCount(numb) {
+    this.count += numb;
+  },
+
+  calculateItemPrice() {
+    return this.items.reduce((acc, item) => {
+      return acc + item.price * item.quantity;
+    }, 0).toFixed(2);
+  },
+
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
+
+  print() {
+    console.log(`\nТовары: ${JSON.stringify(this.items)}`);
+    console.log(`Общая стоимость корзины: ${this.totalPrice} руб.\n`);
+  },
+};
+
+// Изначальное состояние объекта
+console.log('Исходное состояние объекта: ', '\n', cart);
+
+// Тест add() для 3-х товаров
+cart.add('Молоко 1л', 75.9, 50);
+console.log('\n', 'Добавление 1-го товара: ', '\n', cart, '\n');
+cart.add('Кофе молотый 250г', 290.75, 30);
+console.log('\n', 'Добавление 2-го товара: ', '\n', cart, '\n');
+cart.add('Ноутбук ASUS Vivobook Go 15', 53990.0);
+console.log('\n', 'Добавление 3-го товара: ', '\n', cart, '\n');
+
+// Тест ошибочных входных данных
+cart.add(12345, 75.9, 50);
+cart.add("TEST", "TEST");
+cart.add("TEST", 750.78, "TEST");
+
+// Тест print()
+cart.print();
+
+// Тест clear()
+cart.clear();
+console.log("После очистки: ");
+console.log(cart);
+
+// Попытка изменить totalPrice напрямую
+cart.totalPrice = 10; // Не сработает, так как это теперь геттер
+console.log(`\nПопытка изменить totalPrice: ${cart.totalPrice}`);
---
+++ task10/ipv4_distinct.js
@@ -0,0 +1,18 @@
+const listIPv4 = require('./ipv4');
+
+function uniqueIPs(arr) {
+  if (!Array.isArray(arr)) {
+    return "Вы подали на вход не массив IP-адресов!";
+  }
+  let uniqueIPSet = new Set();
+  arr.forEach((item) => uniqueIPSet.add(item));
+  return uniqueIPSet;
+}
+
+//Тестовый запуск
+console.log(`Количество IP-адресов в исходном массиве: ${listIPv4.length}`);
+
+console.log(`Количество уникальных IP-адресов: ${uniqueIPs(listIPv4).size}`);
+console.log(uniqueIPs(listIPv4));
+
+console.log(uniqueIPs({}));
