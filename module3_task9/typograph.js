d5dca77b7ad321a4f52fac87cb04efc4f681104d
Andrey Kostin <andrey.kostin.111@mail.ru>
2025-02-05 21:11:56 +0300

    [M3][TASK-9*] Add typograph extras

--- module3_task9/typograph.js
+++ module3_task9/typograph.js
@@ -6,21 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const outputTextDiv = document.getElementById('outputText');
 
   const typograph = (text) => {
+
+    //5. Замена кавычек на «ёлочки» &laquo; &raquo;
+    text = text.replace(/\"([^"]+)\"/g, '<span style="color: red;">&amp;laquo;</span>$1<span style="color: red;">&amp;raquo;</span>');
+    text = text.replace(/«([^»]+)»/g, '<span style="color: red;">&amp;laquo;</span>$1<span style="color: red;">&amp;raquo;</span>');
+
     // 1. Неразрывный пробел после предлогов
-    const reg = /(?<=\s|^)(в|без|до|для|за|через|над|по|из|у|около|под|о|про|на|к|перед|при|с|между)\s+/gi;
-    text = text.replace(reg, '$1&nbsp;');
+    const reg = /(?<=\s|^)(в|без|до|за|через|над|по|из|у|около|под|о|про|на|к|перед|при|с|между|во|и|а)\s+/gi;
+    text = text.replace(reg, '$1<span style="color: red;">&amp;nbsp;</span>');
 
     // 3. Заменяем # и № на &#8470;
-    text = text.replace(/#|№/g, '&#8470;');
+    text = text.replace(/#|№/g, '<span style="color: red;">&#8470;</span>');
 
     // 2. Заменяем © на &#169;
-    text = text.replace(/©/g, '&#169;');
+    text = text.replace(/© /g, '<span style="color: red;">&amp;copy;&amp;nbsp;</span>');
 
     // 4. Тире с пробелами заменяем на &#151;
-    text = text.replace(/\s[-–]\s/g, ' &#151; ');
-
-    // 5. Замена кавычек на «ёлочки» &laquo; &raquo;
-    text = text.replace(/"([^"]+)"/g, '&laquo;$1&raquo;');
+    text = text.replace(/\s[-–]\s/g, '<span style="color: red;">&amp;nbsp;&amp;mdash;</span> ');
 
     return text;
   };
@@ -28,7 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
   formatButton.addEventListener('click', () => {
     const userInput = inputTextArea.value;
     const formattedText = typograph(userInput);
-    console.log('formattedText: ', formattedText);
 
     outputTextDiv.innerHTML = formattedText;
   });
